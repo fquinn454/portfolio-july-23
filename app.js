2b8bd572a7052602a1eb1e8c7f3e276936b1e9ab
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-07-11 16:16:52 +0100

    Switching Sections Working

---
+++ app.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSsections = document.querySelector('.main-content');
+
+function pageTransitions(){
+    // button click active class
+    for(let i = 0; i<sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', (event) =>{
+            document.querySelector('.active-btn').classList.remove('active-btn');
+            event.target.classList.add('active-btn');
+        })
+    }
+
+    // Sections active
+    allSsections.addEventListener('click', (event) =>{
+        const id = event.target.dataset.id;
+        if(id){
+            // remove selected from the other buttons
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+    
+            event.target.classList.add('active');
+
+            sections.forEach((section) =>{
+                section.classList.remove('active');
+            });
+            
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+        
+}
+
+
+
+pageTransitions();
