fc577e2493fe38507ca20e9c48a1ae5635a30ef0
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-07-12 15:46:06 +0100

    home section styled

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Following [freeCodeCamp.org Tutorial ](https://www.freecodecamp.org/news/create-a-portfolio-website-using-html-css-javascript/) learning<br> 
 + SCSS: Variables
 + SCSS: Nesting
++ CSS: clip-path
++ CSS: filter
++ CSS: transform
++ CSS: animation
++ CSS: ::before to create a pseudo-element that is the first child of the selected element.
 
 and improving JavaScript understanding to include<br>
 + classList.add()
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function pageTransitions(){
     // button click active class
     for(let i = 0; i<sectBtn.length; i++){
         sectBtn[i].addEventListener('click', (event) =>{
-            document.querySelector('.active-btn').classList.remove('active-btn');
+            document.querySelector('active-btn').classList.remove('active-btn');
             event.target.classList.add('active-btn');
         })
     }
@@ -17,11 +17,11 @@ function pageTransitions(){
         const id = event.target.dataset.id;
         if(id){
             // remove selected from the other buttons
-            sectBtns.forEach((btn) => {
-                btn.classList.remove('active');
+            sectBtn.forEach((btn) => {
+                btn.classList.remove('active-btn');
             });
     
-            event.target.classList.add('active');
+            event.target.classList.add('active-btn');
 
             sections.forEach((section) =>{
                 section.classList.remove('active');
